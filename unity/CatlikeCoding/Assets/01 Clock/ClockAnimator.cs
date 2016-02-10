76e0e414cff1ca9c39bac91faac49385da1c1c96
John Tuttle <jtuttle@craw.cc>
2016-02-09 23:19:34 -0500

    clock tutorial

---
+++ unity/CatlikeCoding/Assets/01 Clock/ClockAnimator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using UnityEngine;
+
+public class ClockAnimator : MonoBehaviour {
+    public Transform hours, minutes, seconds;
+
+    public bool analog;
+
+    private const float 
+        HOURS_TO_DEGREES = 360f / 12f, 
+        MINUTES_TO_DEGREES = 360f / 60f, 
+        SECONDS_TO_DEGREES = 360f / 60f;
+
+    private void Update() {
+        if(analog) {
+            TimeSpan timespan = DateTime.Now.TimeOfDay;
+
+            hours.localRotation = Quaternion.Euler(
+                0f, 0f, (float)timespan.TotalHours * -HOURS_TO_DEGREES);
+            minutes.localRotation = Quaternion.Euler(
+                0f, 0f, (float)timespan.TotalMinutes * -MINUTES_TO_DEGREES);
+            seconds.localRotation = Quaternion.Euler(
+                0f, 0f, (float)timespan.TotalSeconds * -SECONDS_TO_DEGREES);
+        } else {
+            DateTime time = DateTime.Now;
+
+            hours.localRotation =
+                Quaternion.Euler(0f, 0f, time.Hour * -HOURS_TO_DEGREES);
+            minutes.localRotation =
+                Quaternion.Euler(0f, 0f, time.Minute * -MINUTES_TO_DEGREES);
+            seconds.localRotation =
+                Quaternion.Euler(0f, 0f, time.Second * -SECONDS_TO_DEGREES);
+        }
+    }
+}
